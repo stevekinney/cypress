555c6b6c249d474744e82671c2fd455e19145d69
Steve Kinney <hello@stevekinney.net>
2021-11-27 12:15:29 -0700

    Get the sidebar working

---
+++ src/lib/utilities/navigation-link.ts
@@ -0,0 +1,17 @@
+import { page } from '$app/stores';
+
+export const navigationLink = (node: HTMLLinkElement) => {
+	const unsubscribe = page.subscribe(({ path }) => {
+		if (path.startsWith(node.getAttribute('href'))) {
+			node.dispatchEvent(new CustomEvent('routeChange', { detail: true }));
+		} else {
+			node.dispatchEvent(new CustomEvent('routeChange', { detail: false }));
+		}
+	});
+
+	return {
+		destroy() {
+			unsubscribe();
+		}
+	};
+};
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,48 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import '../app.css';
+
+	let applications = [
+		{ name: 'Jetsetter', path: '/jetsetter', active: false },
+		{ name: 'Echo Chamber', path: '/echo-chamber', active: false }
+	];
+
+	$: {
+		const { path } = $page;
+		console.log(path, applications);
+		applications = applications.map((application) => ({
+			...application,
+			active: path.startsWith(application.path)
+		}));
+	}
 </script>
 
-<main class="mx-auto p-4 w-full lg:w-2/3">
-	<slot />
-</main>
+<div class="flex gap-4 min-h-screen">
+	<nav id="table-of-contents">
+		<h3>Applications</h3>
+		<ul>
+			{#each applications as application}
+				<li>
+					<a href={application.path} class:active={application.active}>{application.name}</a>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+	<main class="mx-auto p-4 w-full lg:w-2/3">
+		<slot />
+	</main>
+</div>
+
+<style lang="postcss">
+	nav {
+		@apply p-8 w-48 min-h-screen text-white bg-purple-900 border-r-4 border-purple-400;
+	}
+
+	h3 {
+		@apply mb-4 text-lg border-b-4 border-purple-500;
+	}
+
+	.active {
+		@apply font-bold;
+	}
+</style>
