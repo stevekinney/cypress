02f5ea20e37b96d7db10fb4bacaa4d23bbea7d12
Steve Kinney <hello@stevekinney.net>
2021-11-28 17:01:27 -0700

    Update index.svelte

--- src/routes/jetsetter/index.svelte
+++ src/routes/jetsetter/index.svelte
@@ -16,35 +16,41 @@
 
 <form class="flex gap-2 items-end" on:submit|preventDefault={addItem}>
 	<div class="w-full">
-		<label class="block" for="new-item">Item</label>
+		<label class="block" for="new-item-input">Item</label>
 		<input
 			class="block w-3/4"
-			id="new-item"
+			id="new-item-input"
+			data-test="new-item-input"
 			type="text"
 			placeholder="New Item"
 			required
 			bind:value={newItemName}
 		/>
 	</div>
-	<button class="block w-1/4">Add Item</button>
+	<button class="block w-1/4" disabled={!newItemName} id="add-item" data-test="add-item">
+		Add Item
+	</button>
 </form>
 
 <div class="mt-2">
 	<label for="filter-items">Filter</label>
 	<input
 		id="filter-items"
+		data-test="filter-items"
 		class="block w-full small"
 		placeholder="Filter Items"
 		bind:value={$filter}
 	/>
 </div>
 
-<section class="flex gap-2 my-8">
+<section class="flex gap-2 my-8" data-test="items">
 	<ItemList title="Unpacked" items={$unpacked} />
 	<ItemList title="Packed" items={$packed} />
 </section>
 
 <footer class="flex gap-2 w-full">
-	<button class="block" on:click={markAllAsUnpacked}>Mark All as Unpacked</button>
-	<button class="block" on:click={removeAll}>Remove All</button>
+	<button data-test="mark-all-as-unpacked" class="block" on:click={markAllAsUnpacked}
+		>Mark All as Unpacked</button
+	>
+	<button data-test="remove-all" class="block" on:click={removeAll}>Remove All</button>
 </footer>
