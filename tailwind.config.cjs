3729098f6ec436892592a92d07488f79603fe9cd
Steve Kinney <hello@stevekinney.net>
2021-11-28 17:01:25 -0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,10 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	darkMode: 'class', // This can be 'media' if preferred.
+	mode: 'jit',
+	// Don't add a glob below `public` as Rollup doesn't
+	// recognize them and will rebuild in an infinite loop.
+	purge: ['./src/**/*.svelte', './src/**/*.html', './public/index.html'],
+	theme: {},
+	variants: {},
+	plugins: []
+};
