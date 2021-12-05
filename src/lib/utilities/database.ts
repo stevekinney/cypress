b71d9942219da2429f7ffbaf497c36e81c47c24b
Steve Kinney <hello@stevekinney.net>
2021-12-05 15:46:07 -0700

    Update database.ts

--- src/lib/utilities/database.ts
+++ src/lib/utilities/database.ts
@@ -1,3 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import Prisma, * as PrismaAll from '@prisma/client';
+
+const PrismaClient = Prisma?.PrismaClient || PrismaAll?.PrismaClient;
 
 export const prisma = new PrismaClient();
