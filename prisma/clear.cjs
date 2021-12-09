26a72966dfc25a75ee1f673b14d8d286fbade0c0
Steve Kinney <hello@stevekinney.net>
2021-12-09 06:36:26 -0700

    Set up database seeding and clearing

---
+++ prisma/clear.cjs
@@ -0,0 +1,20 @@
+const Prisma = require('@prisma/client');
+const prisma = new Prisma.PrismaClient();
+
+async function main() {
+	const deletePosts = prisma.post.deleteMany({});
+	const deleteUsers = prisma.user.deleteMany({});
+
+	await prisma.$transaction([deletePosts, deleteUsers]);
+
+	console.log('All data has been cleared!');
+}
+
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
---
+++ prisma/seed.cjs
@@ -0,0 +1,58 @@
+const Prisma = require('@prisma/client');
+const prisma = new Prisma.PrismaClient();
+
+async function main() {
+	const deletePosts = prisma.post.deleteMany({});
+	const deleteUsers = prisma.user.deleteMany({});
+
+	await prisma.$transaction([deletePosts, deleteUsers]);
+
+	console.log('All data has been cleared!');
+
+	const first = await prisma.user.upsert({
+		where: { email: 'first@example.com' },
+		update: {},
+		create: {
+			email: 'first@example.com',
+			password: 'password123',
+			posts: {
+				create: [
+					{
+						content: 'Ravioli are a form of Pop Tart.'
+					},
+					{
+						content: 'Taco Bell is the premier fast food option.'
+					}
+				]
+			}
+		}
+	});
+
+	const second = await prisma.user.upsert({
+		where: { email: 'second@example.com' },
+		update: {},
+		create: {
+			email: 'second@example.com',
+			password: 'password456',
+			posts: {
+				create: [
+					{
+						content: 'Thanos had a point.'
+					},
+					{
+						content: 'Winters in Minneapolis are delighful.'
+					}
+				]
+			}
+		}
+	});
+}
+
+main()
+	.catch((e) => {
+		console.error(e);
+		process.exit(1);
+	})
+	.finally(async () => {
+		await prisma.$disconnect();
+	});
