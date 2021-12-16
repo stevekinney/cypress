58740bc87a5ee47579db772f49f1bfca44f24b82
Steve Kinney <hello@stevekinney.net>
2021-12-16 09:22:27 -0800

    Remove profiles

---
+++ prisma/migrations/20211216172213_remove_profiles/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Profile` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- DropTable
+DROP TABLE "Profile";
