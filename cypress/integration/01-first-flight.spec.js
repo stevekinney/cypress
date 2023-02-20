d21a8d2b164930f1bb32f09ba82c8fb623bac400
Paulina <paulina@MacBook-Pro-Paulina.local>
2023-02-20 20:32:34 +0100

    first test

--- cypress/integration/01-first-flight.spec.js
+++ cypress/integration/01-first-flight.spec.js
@@ -1,3 +1,19 @@
 /// <reference types="cypress" />
 
-describe('Create a New Item', () => {});
+describe('Create a New Item', () => {
+    beforeEach(() => {
+        cy.visit('/jetsetter');
+    })
+
+    it('Get form and check if it exists', () => {
+        cy.get('form').should('exist');
+    });
+
+    it('Check if page contains "Add item"', () => {
+        cy.get('form button').contains('Add Item')
+    });
+
+    it('Page loads and error does not exist' , () => {
+        cy.get('.error-page').should('not.exist');
+    })
+});
